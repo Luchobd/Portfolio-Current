5b86fcfdf799ff9e982a5ae961b611e6ed7d3593
Luis Blanco <luiswblancod@gmail.com>
2024-12-07 12:42:26 -0500

    feat: create footer

--- src/pages/Footer/index.tsx
+++ src/pages/Footer/index.tsx
@@ -1,7 +1,29 @@
+import { Fragment, useContext } from "react";
+import { PortfolioContext } from "../../context/Context";
+import { HomeIcons, homeIcons } from "../../utils/Icons/home/home-icons";
+import "./styles.css";
+
 export function Footer() {
+  const { portfolioState } = useContext(PortfolioContext);
+
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="footer-container">
+      <section className="footer-social">
+        {homeIcons.map((social: HomeIcons) => (
+          <Fragment key={social.id}>
+            <a href={social.href} className="footer-network-icon" target="_blank" title={social.title}>
+              <i>{social.icon}</i>
+            </a>
+          </Fragment>
+        ))}
+      </section>
+      <section>
+        {portfolioState.language === "en" ? (
+          <p className="footer-reserved-text">Create by Luis Blanco | All rights reserved</p>
+        ) : (
+          <p className="footer-reserved-text">Creado por Luis Blanco | todos los derechos reservados</p>
+        )}
+      </section>
     </div>
   );
 }
